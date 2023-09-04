3d9dfe99cfe64434c4ea4041505fffaa0ab88275
Fisher Coder <fishercoder@gmail.com>
2023-09-04 09:16:16 -0700

    initial commit for party-parrot project

---
+++ README.md
@@ -0,0 +1,4 @@
+# jsun-party-parrot
+This is an example project forked from https://github.com/flounder4130/party-parrot.
+
+Somehow I cannot get the above repo configured properly in my Intellij, so I just copied all files into my newly created Gradle project in my Intellij.
---
+++ src/main/java/Recolor.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class Recolor {
+    private static final Predicate<Color> mask = c -> c.getAlpha() > 200 && c.getGreen() > 150;
+
+    static final ArrayList<Color> rainbowColors = new ArrayList<>() {{
+        for (int i = 0; i < Constants.HUE_COARSENESS; i++) {
+            float hue = (float) 1 / Constants.HUE_COARSENESS * i;
+            int rgb = Color.HSBtoRGB(hue, 1.0f, 1.0f);
+            add(new Color(rgb));
+        }
+    }};
+
+
+    public static BufferedImage recolor(BufferedImage in, int newColor) {
+        int xSize = in.getWidth();
+        int ySize = in.getHeight();
+        BufferedImage out = new BufferedImage(xSize, ySize, BufferedImage.TYPE_INT_ARGB);
+        for (int x = 0; x < xSize; x++) {
+            for (int y = 0; y < ySize; y++) {
+                Color originalColor = new Color(in.getRGB(x, y), true);
+                if (mask.test(originalColor)) {
+                    out.setRGB(x, y, getRgbForHue(newColor));
+                } else {
+                    out.setRGB(x, y, originalColor.getRGB());
+                }
+            }
+        }
+        return out;
+    }
+
+    private static int getRgbForHue(int hue) {
+        return rainbowColors.get(hue - 1).getRGB();
+    }
+
+}
